150cff036539a3b3669ebde04994530b49885e15
Roman <82457587+phpRulit@users.noreply.github.com>
2021-09-11 21:13:10 +0300

    Update AppMessenger.php

--- api/app/Services/AppMessenger/AppMessenger.php
+++ api/app/Services/AppMessenger/AppMessenger.php
@@ -7,7 +7,7 @@ use App\Services\AppMessenger\Messengers\SmsMessenger;
 
 class AppMessenger implements MessengerInterface
 {
-    //Это наш класс - делегат, с которым будет работать сервис
+    //Это класс делегат, с которым будет работать сервис (patterns design)
 
     /**
      * @var MessengerInterface
