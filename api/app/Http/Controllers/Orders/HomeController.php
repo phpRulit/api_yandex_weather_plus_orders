e5edb5f64c5ce937b30794d848e7b5bbd39ea91c
Roman Bestfator <rbest1981@gmail.com>
2021-09-11 12:58:45 +0300

    some edit

--- api/app/Http/Controllers/Orders/HomeController.php
+++ api/app/Http/Controllers/Orders/HomeController.php
@@ -15,23 +15,25 @@ class HomeController extends Controller
     {
         $query = Order::with(['partner', 'orderProducts', 'products']);
 
-        if (!empty($request['status']) && $request['status'] === 'all') {
-            $query->orderByDesc('created_at');
-        } elseif (!empty($request['status']) && $request['status'] === 'overdue') {
-            $query->where('status', Order::STATUS_CONFIRM)
-                ->orderByDesc('delivery_dt');
-        } elseif (!empty($request['status']) && $request['status'] === 'current') {
-            $query->where('status', Order::STATUS_CONFIRM)
-                ->where('delivery_dt', '>=', Carbon::now()->subDay())
-                ->orderBy('delivery_dt');
-        } elseif (!empty($request['status']) && $request['status'] === 'new') {
-            $query->where('status', Order::STATUS_NEW)
-                ->where('delivery_dt', '<', Carbon::now()->subDay())
-                ->orderBy('delivery_dt');;
-        } elseif (!empty($request['status']) && $request['status'] === 'completed') {
-            $query->where('status', Order::STATUS_COMPLETED)
-                ->where('delivery_dt', '>', Carbon::today())
-                ->orderByDesc('delivery_dt');
+        if (!empty($request['status'])) {
+            if ($request['status'] === 'all') {
+                $query->orderByDesc('created_at');
+            } elseif ($request['status'] === 'overdue') {
+                $query->where('status', Order::STATUS_CONFIRM)
+                    ->orderByDesc('delivery_dt');
+            } elseif ($request['status'] === 'current') {
+                $query->where('status', Order::STATUS_CONFIRM)
+                    ->where('delivery_dt', '>=', Carbon::now()->subDay())
+                    ->orderBy('delivery_dt');
+            } elseif ($request['status'] === 'new') {
+                $query->where('status', Order::STATUS_NEW)
+                    ->where('delivery_dt', '<', Carbon::now()->subDay())
+                    ->orderBy('delivery_dt');;
+            } elseif ($request['status'] === 'completed') {
+                $query->where('status', Order::STATUS_COMPLETED)
+                    ->where('delivery_dt', '>', Carbon::today())
+                    ->orderByDesc('delivery_dt');
+            }
         }
 
         return response()->json($query->has('orderProducts')->paginate($request['max']));
