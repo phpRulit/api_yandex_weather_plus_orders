2fba0fc5a815d53698aadf6a6f59ed7d659e883d
Roman Bestfator <rbest1981@gmail.com>
2021-09-15 14:44:08 +0300

    some edit

--- api/app/Services/Order/OrderService.php
+++ api/app/Services/Order/OrderService.php
@@ -21,7 +21,13 @@ class OrderService
 
     public function editOrder(Order $order, EditOrderRequest $request): void
     {
-        $order->fill($request->all())->save();
+        if (isset($request['status']) && $order->status !== (int)$request['status']) {
+            $order->update(['status' => (int)$request['status']]);
+        } elseif (isset($request['client_email']) && $order->client_email !== $request['client_email']) {
+            $order->update(['client_email' => $request['client_email']]);
+        } elseif (isset($request['delivery_dt']) && $order->delivery_dt !== $request['delivery_dt']) {
+            $order->update(['delivery_dt' => $request['delivery_dt']]);
+        }
     }
 
     public function sentMailsAboutOrderCompleted(Order $order): void
